451f2c3ec194270407ab4af622b043242254b02d
Stefano Verna <stefano.verna@welaika.com>
2012-09-19 11:28:33 +0200

    Makes attributes attr_accessible

--- lib/railsyard/globalize/active_record_extension.rb
+++ lib/railsyard/globalize/active_record_extension.rb
@@ -2,10 +2,12 @@ module Railsyard::Globalize
   module ActiveRecordExtension
 
     def railsyard_translates(*args, &block)
+      attrs = args.dup.extract_options!
       translates(*args)
       translation_class.instance_eval &block
       translation_class.instance_eval do
         attr_accessible :locale
+        attr_accessible *attrs
       end
 
       attr_accessible :translations_attributes
