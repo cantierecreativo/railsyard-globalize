c6ae05d1db1455d8680370d66b4953a0fa18c47f
Stefano Verna <stefano.verna@welaika.com>
2012-09-19 11:21:42 +0200

    Makes locale attr_accessible

--- lib/railsyard/globalize/active_record_extension.rb
+++ lib/railsyard/globalize/active_record_extension.rb
@@ -4,6 +4,9 @@ module Railsyard::Globalize
     def railsyard_translates(*args, &block)
       translates(*args)
       translation_class.instance_eval &block
+      translation_class.instance_eval do
+        attr_accessible :locale
+      end
 
       attr_accessible :translations_attributes
       accepts_nested_attributes_for :translations, allow_destroy: true
